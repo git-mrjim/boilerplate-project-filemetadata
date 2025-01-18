eb813108417bd81d3fb5b4c0fbd988f2c272d986
Jimwell Ibay <ibayjimwell@gmail.com>
2025-01-18 06:07:12 +0000

    API Project: File Metadata Microservice

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+
+const upload = multer({
+  dest: 'uploads/'
+});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +16,20 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
 
+  if (!file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
+
+});
 
 
 const port = process.env.PORT || 3000;
